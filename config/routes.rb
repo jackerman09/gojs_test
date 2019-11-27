f8088d2ce85dbf238602e12d37659dfd5eeed75f
Jackerman09 <jackerman09@gmail.com>
2019-11-27 14:04:05 -0500

    update show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root  'landings#index'
   resources :entities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
