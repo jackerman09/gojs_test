28e8995c1d7bd6e7ac39dab3d072efb974078387
Jackerman09 <jackerman09@gmail.com>
2019-11-27 13:53:17 -0500

    fix relationships, update index/show

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,7 @@
 class Entity < ApplicationRecord
   has_many :ownerships_as_parent, :foreign_key => "parent_id", :class_name => "Ownership"
-  has_many :parents, :through => :ownerships_as_parent
-  
+  has_many :subsidiaries, :through => :ownerships_as_parent
+
   has_many :ownerships_as_subsidiary, :foreign_key => "subsidiary_id", :class_name => "Ownership"
-  has_many :subsidiaries, :through => :ownerships_as_subsidiary
+  has_many :parents, :through => :ownerships_as_subsidiary
 end
