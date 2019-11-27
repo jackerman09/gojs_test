d9a3256fbcd3d6ea9b1621f1681142817b16607b
Jackerman09 <jackerman09@gmail.com>
2019-11-27 13:26:48 -0500

    set up many-to-many self join

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,8 +1,7 @@
 class Entity < ApplicationRecord
-	has_many :owners, class_name: "Ownership",
-										foreign_key: "owner_id",
-										dependent: :destroy
-  has_many :subsidiaries, class_name: "Ownership",
-  												foreign_key: "subsidiary_id",
-  												dependent: :destroy
+  has_many :ownerships_as_parent, :foreign_key => "parent_id", :class_name => "Ownership"
+  has_many :parents, :through => :ownerships_as_parent
+  
+  has_many :ownerships_as_subsidiary, :foreign_key => "subsidiary_id", :class_name => "Ownership"
+  has_many :subsidiaries, :through => :ownerships_as_subsidiary
 end
--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,4 @@
 class Ownership < ApplicationRecord
-	belongs_to :owner, class_name: "Entity"
-	belongs_to :subsidiary, class_name: "Entity"
+	belongs_to :parent, foreign_key: "parent_id", class_name: "Entity"
+	belongs_to :subsidiary, foreign_key: "subsidiary_id", class_name: "Entity"
 end
